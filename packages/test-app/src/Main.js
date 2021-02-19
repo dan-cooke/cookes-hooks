0ee056aa873e9d919bb4cad0bf46f9b384a78f94
dan-cooke <danielcooke@1996@gmail.com>
2021-02-19 16:57:38 +0000

    chore: update npmrc

--- packages/test-app/src/Main.js
+++ packages/test-app/src/Main.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useDelayedFunctionQueue  from '@cookes-hooks/useDelayedFunctionQueue'
 
 const Main = () => {
     const { add, queue } = useDelayedFunctionQueue()
-    add();
-    queue();
     return <div></div>
 }
 
